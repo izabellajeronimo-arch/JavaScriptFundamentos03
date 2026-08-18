d1febe45ec9c9072715bf600da362fc2536d1ebf
IZABELLA NEVES PIVARO JERONIMO <26171802@edu119.local>
2026-08-18 11:32:32 -0300

    Atualizando Projeto

---
+++ 01-funcoes/05-funcao.condicional.js
@@ -0,0 +1,10 @@
+function verificandoNota(media) {
+    if(media >= 7){
+        return "Aprovado"
+    }
+    return "Reprovado"
+
+    return media >= 7 ? "Aprovado" : "Reprovado"
+}
+
+consolelog(verificandoNota(8));
---
+++ 01-funcoes/06-funcition-expression.js
@@ -0,0 +1,10 @@
+function somarTradicional(a, b){
+    return a + b;
+}
+
+const somarAnonima = function (a, b) {
+    return a + b;
+}
+
+console.log(somarTRadicional(10,5));
+console.log(somarAnonima(10,5));
