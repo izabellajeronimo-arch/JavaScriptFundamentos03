36136ba513375635babe519127a1c51dc7841c96
IZABELLA NEVES PIVARO JERONIMO <26171802@edu119.local>
2026-08-18 14:09:07 -0300

    atuzalizando projeto

---
+++ 01-funcoes/07-arrow-function.js
@@ -0,0 +1,14 @@
+function somarTradicional(a, b){
+    return a + b;
+}
+
+const somarAnonima = function (a, b){
+    return a + b;
+}
+
+const arrowFunction = (a,b) =>{
+    return a + b
+}
+console.log(somarTradicional(10,5));
+console.log(somarAnonima(10,5));
+console.log(arrowFunction(10, 5))
---
+++ 02-metodos-arrays/05-find-index.js
@@ -0,0 +1,5 @@
+let nome = ["João", "Júlia", "Larissa", "Lívia"];
+
+const índice = nomes.findIndex((n) => n === "Larissa");
+
+console.log(índice);
