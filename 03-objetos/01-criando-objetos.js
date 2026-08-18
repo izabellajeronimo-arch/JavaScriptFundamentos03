ef29a0558f00c509615559e9f371732fbc604a57
IZABELLA NEVES PIVARO JERONIMO <26171802@edu119.local>
2026-08-18 16:06:35 -0300

    atuzalizando projeto

---
+++ 03-objetos/01-criando-objetos.js
@@ -0,0 +1,12 @@
+const aluno = {
+    nome: "Carlos",
+    idade: 17,
+    nota: 8
+};
+
+// Java
+// Script
+// Object
+// Notation
+
+console.log(aluno);
--- 03-objetos/04-metodos-objetivos.js
+++ 03-objetos/04-metodos-objetivos.js
@@ -5,4 +5,4 @@ const aluno ={
     }
 };
 
-aluno.apresentar();
+alunos.apresentar();
