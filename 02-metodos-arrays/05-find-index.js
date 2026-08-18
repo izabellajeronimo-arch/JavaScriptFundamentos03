e4f7d280e56d4c54813aba722a993ab410a12344
IZABELLA NEVES PIVARO JERONIMO <26171802@edu119.local>
2026-08-18 14:14:27 -0300

    atuzalizando projeto

--- 02-metodos-arrays/05-find-index.js
+++ 02-metodos-arrays/05-find-index.js
@@ -3,3 +3,7 @@ let nome = ["João", "Júlia", "Larissa", "Lívia"];
 const índice = nomes.findIndex((n) => n === "Larissa");
 
 console.log(índice);
+
+const indice1 = nomes.findIndex((n) => n === "Matheus");
+
+console.log(indice1);
